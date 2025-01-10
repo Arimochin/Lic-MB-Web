b1bcf0887586e4adc69ad8cb918e6c8513c49cce
Sofía Todesco <166171810+Arimochin@users.noreply.github.com>
2025-01-09 23:11:17 -0300

    Upload of the images on the submit-form
    Se mandan las imagenes a una carpeta y en la base queda el nombre del archivo con su extension.

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -11,13 +11,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $adress = filter_var($_POST['adress'], FILTER_UNSAFE_RAW);
     $phone_number = filter_var($_POST['phone-number'], FILTER_UNSAFE_RAW);
     $os = filter_var($_POST['os'], FILTER_UNSAFE_RAW);
+    
+    // Codigo para las imagenes
+    $filenameDerMed = $_FILES["derMed"]["name"];
+    $tempnameDerMed = $_FILES["derMed"]["tmp_name"];
+    $folderDerMed = __DIR__ . "/../derMed-images/". $filenameDerMed;
 
+    if (move_uploaded_file($tempnameDerMed, $folderDerMed)){
+        echo "<h3>&nbsp; Imagen subida correctamente!</h3>";
+    } else {
+        echo "<h3>&nbsp; Falló en subirse la imagen!</h3>";
+    }
 
+    $filenameAutObS = $_FILES["autObS"]["name"];
+    $tempnameAutObS = $_FILES["autObS"]["tmp_name"];
+    $folderAutObS = __DIR__ . "/../autObS-images/". $filenameAutObS;
+
+    if (move_uploaded_file($tempnameAutObS, $folderAutObS)){
+        echo "<h3>&nbsp; Imagen subida correctamente!</h3>";
+    } else {
+        echo "<h3>&nbsp; Falló en subirse la imagen!</h3>";
+    }
+
+    
     // Asegurarse de que la conexión esté activa
     if ($conn) {
         // Preparar la consulta de inserción
-        $query = "INSERT INTO DATOS_PERSONALES (dni, firstname, secondname, adress, phone, os)
-                  VALUES ($1, $2, $3, $4, $5, $6 )"; // Asegurarse de tener todos los 8 parámetros
+        $query = "INSERT INTO DATOS_PERSONALES (dni, firstname, secondname, adress, phone, os, dev, autor)
+                  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)"; // Asegurarse de tener todos los 8 parámetros
 
         // Ejecutar la consulta con parámetros
         $result = pg_query_params($conn, $query, [
@@ -26,7 +47,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $secondname,
             $adress,
             $phone_number,
-            $os
+            $os,
+            $filenameDerMed,
+            $filenameAutObS
         ]);
 
         // Comprobar el resultado
